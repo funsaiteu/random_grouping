da7fccaffccf899d08531cfd5dfdd888d8a84af6
funsaiteu <abcdefghijk_e_i_o@icloud.com>
2021-06-05 18:00:09 +0900

    implemented random seed option

--- random_grouping.py
+++ random_grouping.py
@@ -3,7 +3,7 @@
 
 
 '''
-Last modified: Sat, 05 Jun 2021 17:51:58 +0900
+Last modified: Sat, 05 Jun 2021 17:56:10 +0900
 author: Takaya Saito
 github: funsaiteu
 Sorry for the dirty hack.
@@ -17,15 +17,17 @@ import numpy as np
 
 
 def main():
-    parser = argparse.ArgumentParser(description='for online party(e.g. % ./random_grouping.py -n 3 -m ./member)')
+    parser = argparse.ArgumentParser(description='for online party(e.g. % ./random_grouping.py -n 3 -m ./member -r 0)')
     parser.add_argument('-n', type=int, default='3', help="number of split u wanna(default='3')")
     parser.add_argument('-m', default='./member', help="member list's path(default='./member')")
+    parser.add_argument('-r', type=int, default='0', help="random seed(default='0')")
     args = parser.parse_args()
     N = args.n
     path = pathlib.Path(args.m)
+    seed = args.r
     with path.open() as f:
         member_list = [s.strip() for s in f.readlines()]
-    random.seed(0)
+    random.seed(seed)
     member_list_random = random.sample(member_list, len(member_list))
     for i, name in enumerate(np.array_split(member_list_random, N)):
         print(f'group{i}: {name}')
